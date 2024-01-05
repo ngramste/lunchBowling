9acdcda15164bc675a62d34a7e82e9e5b9a7b013
Nick <ngramste@gmail.com>
2024-01-05 16:36:55 -0600

    Output more data

--- cryingTowel.py
+++ cryingTowel.py
@@ -27,28 +27,43 @@ for bowler in report:
 men = {'name': '', 'drop': 0.0}
 women = {'name': '', 'drop': 0.0}
 unknown = []
+all = []
 
 for bowler in report:
+    data = {'name': bowler, 'drop': 0}
+    
     if 'mw' not in report[bowler]:
+      data['mw'] = 'U'
       if len(report[bowler]['weeks']) > 0:
-        data = {'name': bowler, 'drop': 0.0}
         for week in report[bowler]['weeks']:
           if 'changeInAve' in week:
             if week['changeInAve'] < data['drop']:
               data['drop'] = week['changeInAve']
         unknown.append(data)
+      else:
+        data['drop'] = 0
+        
     elif report[bowler]['mw'] == 'M':
+      data['mw'] = 'M'
       for week in report[bowler]['weeks']:
         if 'changeInAve' in week:
+          if week['changeInAve'] < data['drop']:
+            data['drop'] = week['changeInAve']
           if week['changeInAve'] < men['drop']:
             men['drop'] = week['changeInAve']
             men['name'] = bowler
+            
     elif report[bowler]['mw'] == 'W':
+      data['mw'] = 'W'
       for week in report[bowler]['weeks']:
         if 'changeInAve' in week:
+          if week['changeInAve'] < data['drop']:
+            data['drop'] = week['changeInAve']
           if week['changeInAve'] < women['drop']:
             women['drop'] = week['changeInAve']
             women['name'] = bowler
+            
+    all.append(data)
   
 print(json.dumps(men))
 print(json.dumps(women))
@@ -56,4 +71,8 @@ print(json.dumps(unknown))
 
 fd = open(c.DATA_FOLDER + "report-cryData.json", "w")
 fd.write(json.dumps(report))
+fd.close()
+
+fd = open(c.DATA_FOLDER + "cryingTowel.json", "w")
+fd.write(json.dumps(all))
 fd.close()
