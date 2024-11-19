5474287705eae011f603b5f598eded5d74075a80
Ramstead <E10657871@adxuser.com>
2024-11-19 16:23:13 -0600

    Added commenting features
    At least for the more complex scripts that I will likely have to edit in the future

--- calculateStandings.py
+++ calculateStandings.py
@@ -5,46 +5,73 @@ import re
 
 teamScores = {}
 
+# We need to reference a lot of local files to figure everything out, start with getting basic team data
 with open(c.TEAMS_PATH) as json_teams:
   report_teams = json.load(json_teams)
-  
-  # teamNum is the value reported in the schedule
 
+  # Now lets open the season schedule for reference
   with open(c.SCHEDULE_PATH) as json_scedule:
     report_scedule = json.load(json_scedule)
 
+    # Loop over all the recorded results, these reports hold the scores for each week
     for filename in listdir(c.SUMMARY_PATH):
       with open(c.SUMMARY_PATH + "\\" + filename) as json_data:
         report = json.load(json_data)
         week = re.findall(r'\d+', filename)[0]
+        
+        # Build the array of arrays for weekly matchups
         weekMatchups = [weekData for weekData in report_scedule['weeks'] if weekData['weekNum'] == int(week)][0]['matchups']
         
+        # Loop over each matchup for this week
         for matchup in weekMatchups:
+          # And loop over the two teams in this matchup
           for teamNum in matchup:
+            # Convert from a team nuber to a team name
             teamName = list(filter(lambda team: team['TeamNum'] == teamNum, report_teams['Data']))[0]['TeamName']
               
+            # This is the first week, add the blank placeholder data for this team
             if teamName not in teamScores:
-              
               teamScores[teamName] = {
                 'score': 0,
                 'handicapPins': 0
               }
               
+            # Store the opponents name and team number for later reference
             opponentNum = list(filter(lambda team: team != teamNum, matchup))[0]
+            opponentName = list(filter(lambda team: team['TeamNum'] == opponentNum, report_teams['Data']))[0]['TeamName']
             
+            # Filter out the weekly results to find the bowlers on the teams in question
             bowlers = list(filter(lambda bowler: bowler['TeamNum'] == teamNum, report['Data']))
             opponents = list(filter(lambda bowler: bowler['TeamNum'] == opponentNum, report['Data']))
             
+            # Figure out what the team in question scored
             game1 = bowlers[0]['Score1'] + bowlers[0]['HandicapBeforeBowling'] + bowlers[1]['Score1'] + bowlers[1]['HandicapBeforeBowling']
             game2 = bowlers[0]['Score2'] + bowlers[0]['HandicapBeforeBowling'] + bowlers[1]['Score2'] + bowlers[1]['HandicapBeforeBowling']
             series = game1 + game2
             
+            # Figure out what the opponents scored
             oppoentGame1 = opponents[0]['Score1'] + opponents[0]['HandicapBeforeBowling'] + opponents[1]['Score1'] + opponents[1]['HandicapBeforeBowling']
             oppoentGame2 = opponents[0]['Score2'] + opponents[0]['HandicapBeforeBowling'] + opponents[1]['Score2'] + opponents[1]['HandicapBeforeBowling']
             opponentsSeries = oppoentGame1 + oppoentGame2
             
+            # Just code for debugging, uncomment to see the stats for a single team for the season
+            # if teamNum == 28:
+            #   print(json.dumps({
+            #     'week': filename,
+            #     'name': teamName,
+            #     'opponent': opponentName,
+            #     'game1': game1,
+            #     'oppoentGame1': oppoentGame1,
+            #     'game2': game2,
+            #     'oppoentGame2': oppoentGame2,
+            #     'series': series,
+            #     'opponentsSeries': opponentsSeries
+            #   }, indent=2))
+            
+            # Add in the handicap series to the team score data
             teamScores[teamName]['handicapPins'] += series
             
+            # These if statements will add up the points for this week
             if game1 > oppoentGame1:
               teamScores[teamName]['score'] += 1
             
@@ -63,9 +90,11 @@ with open(c.TEAMS_PATH) as json_teams:
             if series == opponentsSeries:
               teamScores[teamName]['score'] += 0.5
               
+# Sort the scores from first place to last
 sortedScores = dict(sorted(teamScores.items(), key=lambda item: (item[1]['score'], item[1]['handicapPins']), reverse = True))
 print(json.dumps(sortedScores, indent=2))
 
+# Write the results out to file for safe keeping
 fd = open(c.DATA_FOLDER + "standings.json", "w")
 fd.write(json.dumps(sortedScores, indent=2))
 fd.close()
--- scrapeData.py
+++ scrapeData.py
@@ -4,18 +4,25 @@ from datetime import datetime
 import constants as c
 from os import listdir
 
+# Open the schedule, we will use this to see what weeks might have data to scrape
 with open(c.SCHEDULE_PATH) as json_data:
   schedule = json.load(json_data)
 
+# Get a list of files which represent weeks we previously scraped for
 downloaded = listdir(c.SUMMARY_PATH)
   
+# Loop over all the weeks in the schedule
 for week in schedule["weeks"]:
+  # If the week in question happend today or in the past, we will try to scrape it
   date = datetime.strptime(week["date"], '%m/%d/%Y')
   if date < datetime.now():
     filename = "week" + str(week["weekNum"]) + ".json"
+    
+    # Before scraping, make sure we don't already have the file downloaded
     if not filename in downloaded:
       print("Retrieving data for " + filename)
     
+      # Build the post request for the secret league secretary API call
       postData = {
         "leagueId": c.leagueId,
         "year": c.year,
@@ -23,11 +30,14 @@ for week in schedule["weeks"]:
         "weekNum": week["weekNum"]
       }
       
+      # Get that juicy juicy data!
       request = requests.post(c.Summary_Read, postData)
       
+      # If the results are empty, Leauge Secretary doesn't have the results yet
       if request.json()['Total'] == 0:
         print("Results for week " + str(week["weekNum"]) + " have not been published")
       
+      # If there is data, write it to a file
       else:
         fd = open(c.SUMMARY_PATH + "/" + filename, "w")
         fd.write(json.dumps(request.json(), indent=2))
